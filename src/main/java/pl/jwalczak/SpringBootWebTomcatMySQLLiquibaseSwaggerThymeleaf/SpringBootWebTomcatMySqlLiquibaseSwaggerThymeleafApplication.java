e98da40078a4abffb3657859d77dab9ff024f500
Jakub Walczak <jacob.walczak89@gmail.com>
2022-07-28 20:17:09 +0200

    Init commit from Spring Boot Initializer

---
+++ src/main/java/pl/jwalczak/SpringBootWebTomcatMySQLLiquibaseSwaggerThymeleaf/SpringBootWebTomcatMySqlLiquibaseSwaggerThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pl.jwalczak.SpringBootWebTomcatMySQLLiquibaseSwaggerThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTomcatMySqlLiquibaseSwaggerThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTomcatMySqlLiquibaseSwaggerThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jwalczak/SpringBootWebTomcatMySQLLiquibaseSwaggerThymeleaf/SpringBootWebTomcatMySqlLiquibaseSwaggerThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jwalczak.SpringBootWebTomcatMySQLLiquibaseSwaggerThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTomcatMySqlLiquibaseSwaggerThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
